3cb12055e29041227bebfe5b9270befeed90c756
Arles Asaeli <arles.halley@gmail.com>
2021-06-07 09:36:03 +0700

    Table view

---
+++ Addictech/Analogy.swift
@@ -0,0 +1,21 @@
+//
+//  Analogy.swift
+//  Addictech
+//
+//  Created by Arles Halley on 07/06/21.
+//
+
+import Foundation
+
+struct analogy {
+    let terminology: String
+    let description: String
+}
+
+//struct analogyList {
+//    static let = analogyArray [
+//        analogy(
+//    
+//    )
+//    ]
+//}
