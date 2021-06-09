e11fa6d9f9e9499edd89a26676b3f5502915936d
Arles Asaeli <arles.halley@gmail.com>
2021-06-09 19:14:55 +0700

    Buat detailView

--- Addictech/DetailView.swift
+++ Addictech/DetailView.swift
@@ -7,11 +7,14 @@
 
 import SwiftUI
 
-struct DetailView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+//struct DetailView: View {
+//    var body: some View {
+//        NavigationView {
+//            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//                .navigationTitle("Definisi")
+//        }
+//    }
+//}
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
