1a620adc44d1d08d41db88e6201820a6cd9dad5a
Arles Asaeli <arles.halley@gmail.com>
2021-06-11 06:58:29 +0700

    ...

--- Addictech/View/MainView.swift
+++ Addictech/View/MainView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 // var test = "testing"
 
-
 struct MainView: View {
     @State private var searchText : String = ""
     @State var showInfoModalView: Bool = false
@@ -20,64 +19,57 @@ struct MainView: View {
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         UITableView.appearance().contentInset.top = -35
-     
-        
-        
-//        UISearchBar.appearance().backgroundColor = #colorLiteral(red: 0, green: 0.1807721257, blue: 0.4193686843, alpha: 1)
-//        UITableView.appearance().backgroundColor = #colorLiteral(red: 0, green: 0.1807721257, blue: 0.4193686843, alpha: 1)
-        
+
+        //        UISearchBar.appearance().backgroundColor = #colorLiteral(red: 0, green: 0.1807721257, blue: 0.4193686843, alpha: 1)
+        //        UITableView.appearance().backgroundColor = #colorLiteral(red: 0, green: 0.1807721257, blue: 0.4193686843, alpha: 1)
+
     }
-   
     
     var body: some View {
         NavigationView {
-            ZStack{
-                Color("DarkBlue")
-                    .ignoresSafeArea()
-            VStack{
-            HStack{
-                SearchBar(text: $searchText)
-                Button(action: {showInfoModalView = true
-                    
-                }) {
-                    Image(systemName: "line.horizontal.3.decrease.circle").imageScale(.large)
-                        .foregroundColor(Color.white)
-                }
-            }
-            .padding(/*@START_MENU_TOKEN@*/[.leading, .bottom, .trailing], 9.0/*@END_MENU_TOKEN@*/)
-            
-                
-            
                 ZStack{
-                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/21.0/*@END_MENU_TOKEN@*/)
-                        .foregroundColor(Color("Color"))
+                    Color("DarkBlue")
                         .ignoresSafeArea()
-                List {
-                
-                    ForEach(self.fetchDictionaries.dictionaryData.filter {
-                        self.searchText.isEmpty ? true : $0.keywords.lowercased().contains(self.searchText.lowercased())
-                    }, id: \.keywords){item in
-                        NavigationLink(
-                            destination: DetailView(dictionary: item),
-                            label: {
-                                Text(item.keywords)
-                            })
+                    VStack{
+                        HStack{
+                            SearchBar(text: $searchText)
+                            Button(action: {showInfoModalView = true
+                                
+                            }) {
+                                Image(systemName: "line.horizontal.3.decrease.circle").imageScale(.large)
+                                    .foregroundColor(Color.white)
+                            }
+                        }
+                        .padding(/*@START_MENU_TOKEN@*/[.leading, .bottom, .trailing], 9.0/*@END_MENU_TOKEN@*/)
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/21.0/*@END_MENU_TOKEN@*/)
+                                .foregroundColor(Color("Color"))
+                                .ignoresSafeArea()
+                            List {
+                                
+                                ForEach(self.fetchDictionaries.dictionaryData.filter {
+                                    self.searchText.isEmpty ? true : $0.keywords.lowercased().contains(self.searchText.lowercased())
+                                }, id: \.keywords){item in
+                                    NavigationLink(
+                                        destination: DetailView(dictionary: item),
+                                        label: {
+                                            Text(item.keywords)
+                                        })
+                                }
+                            }
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            .navigationTitle("Dictionary")
+                            .listStyle(GroupedListStyle())
+                        }
+                        .accentColor(Color(.label))
                     }
                 }
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-        .navigationTitle("Dictionary")
-                .listStyle(GroupedListStyle())
-                }
-                .accentColor(Color(.label))
-                
-                
             }
-        }
-        }
-        .sheet(isPresented: $showInfoModalView, content: {
-            
+            .sheet(isPresented: $showInfoModalView, content: {
                 filtermodal()
-        })
+            })
+        
     }
 }
 
