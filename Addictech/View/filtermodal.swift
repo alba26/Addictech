38f16b1c365856a417d561af958fba7235a22d14
Helen I H <helenihermanto@gmail.com>
2021-06-10 09:09:59 +0700

    filter

---
+++ Addictech/View/filtermodal.swift
@@ -0,0 +1,20 @@
+//
+//  filtermodal.swift
+//  Addictech
+//
+//  Created by Helen I H on 10/06/21.
+//
+
+import SwiftUI
+
+struct filtermodal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct filtermodal_Previews: PreviewProvider {
+    static var previews: some View {
+        filtermodal()
+    }
+}
