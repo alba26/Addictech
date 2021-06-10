10829f8a83bcd223fcd52a57edd605f2ed195c0a
Helen I H <helenihermanto@gmail.com>
2021-06-10 09:23:45 +0700

    filter modal neww

--- Addictech/View/filtermodal.swift
+++ Addictech/View/filtermodal.swift
@@ -7,9 +7,66 @@
 
 import SwiftUI
 
+struct FilterButton: ViewModifier {
+
+   
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.body)
+            .padding(12.0)
+            .overlay(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke( Color.black, lineWidth: 2))
+    }
+}
+
+
 struct filtermodal: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20.0){
+            Text("Sort")
+                .font(.body)
+                .fontWeight(.semibold)
+            
+            HStack{
+                Button(action: {}, label: {
+                    Text("Ascending")
+                }).modifier(FilterButton())
+                
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Descending")
+                }).modifier(FilterButton())
+
+            }
+            
+            Text("Category")
+                .font(.body)
+                .fontWeight(.semibold)
+            
+            HStack{
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("ML")
+                }).modifier(FilterButton())
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Computer")
+                }).modifier(FilterButton())
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Website")
+                        
+                }).modifier(FilterButton())
+            }
+            
+            
+        }
+        .padding(.horizontal, 25.0)
+        .frame(width: 330, height: 400.0)
+        .offset(y: -125)
+
+        
     }
 }
 
