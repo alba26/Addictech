1df417701f095f3cde62ef7a7a882ffb70fac79c
alba26199 <adhityalba@gmail.com>
2021-06-11 10:11:04 +0700

    minor

--- Addictech/View/DetailView.swift
+++ Addictech/View/DetailView.swift
@@ -19,85 +19,91 @@ struct DetailView: View {
                         .font(.title)
                         .fontWeight(.bold)
                     Button(action: {
-                                        self.textSpeech.speak(keywords: dictionary.keywords)
-                                    }, label: {
-                                        Image(systemName: "speaker.wave.2.fill")
-                                    })
+                        self.textSpeech.speak(keywords: dictionary.keywords)
+                    }, label: {
+                        Image(systemName: "speaker.wave.2.fill")
+                    })
                     .buttonStyle(BorderlessButtonStyle())
                 }
                 
                 
             }
             .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Section(header: Text("Tech Explanation")){
+                Text(dictionary.tech_explanation)}
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Section(header: Text("Analogy")){
+                if dictionary.image_name == "-"{
+                    Image("potrait_pic")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top)
+                }else{
+                    Image(dictionary.image_name)
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top)
+                }
                 
-                Section(header: Text("Tech Explanation")){
-                    Text(dictionary.tech_explanation)}
-                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                    
-                    Section(header: Text("Analogy")){
-                        Image("potrait_pic")
-                            .resizable()
-                            .scaledToFit()
-                            .padding(.top)
-                            
-                        Text(dictionary.analogy)
-                            
-                    }
-                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-
+                Text(dictionary.analogy)
+                
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
         }
-        
         .listStyle(InsetGroupedListStyle())
         .navigationBarTitle("", displayMode: .inline)
         
         
-//        ScrollView {
-//            VStack{
-//                Button(action: {
-//                    self.textSpeech.speak(keywords: dictionary.keywords)
-//                }, label: {
-//                    Text("Speaker")
-//                })
-//            }
-//            VStack(alignment: .leading, spacing: 15){
-//                Spacer()
-//                ZStack{
-//                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/21.0/*@END_MENU_TOKEN@*/)
-//                        .frame(height: 295.0)
-//                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-//                    VStack(alignment: .leading, spacing: 5){
-//                        Text("Tech Explanation")
-//                            .font(.callout)
-//                            .bold()
-//                        Text(dictionary.tech_explanation)
-//                            .font(.footnote)
-//                    }
-//                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-//                }
-//
-//                VStack(alignment: .leading, spacing: 5){
-//                    ZStack{
-//                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/21.0/*@END_MENU_TOKEN@*/)
-//
-//                            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-//                        VStack(alignment: .leading, spacing: 5.0){
-//                            Text("Analogy")
-//                                .font(.callout)
-//                                .bold()
-//                            Text(dictionary.analogy)
-//                                .font(.footnote)
-//                        }
-//                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-//                    }
-//                }
-//            }
-//            .padding(.leading)
-//            .padding(.trailing)
-//
-//
-//        }
-//        .background(Color("Color"))
-//        .navigationBarTitle(dictionary.keywords, displayMode: .inline)
+        //        ScrollView {
+        //            VStack{
+        //                Button(action: {
+        //                    self.textSpeech.speak(keywords: dictionary.keywords)
+        //                }, label: {
+        //                    Text("Speaker")
+        //                })
+        //            }
+        //            VStack(alignment: .leading, spacing: 15){
+        //                Spacer()
+        //                ZStack{
+        //                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/21.0/*@END_MENU_TOKEN@*/)
+        //                        .frame(height: 295.0)
+        //                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+        //                    VStack(alignment: .leading, spacing: 5){
+        //                        Text("Tech Explanation")
+        //                            .font(.callout)
+        //                            .bold()
+        //                        Text(dictionary.tech_explanation)
+        //                            .font(.footnote)
+        //                    }
+        //                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        //                }
+        //
+        //                VStack(alignment: .leading, spacing: 5){
+        //                    ZStack{
+        //                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/21.0/*@END_MENU_TOKEN@*/)
+        //
+        //                            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+        //                        VStack(alignment: .leading, spacing: 5.0){
+        //                            Text("Analogy")
+        //                                .font(.callout)
+        //                                .bold()
+        //                            Text(dictionary.analogy)
+        //                                .font(.footnote)
+        //                        }
+        //                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        //                    }
+        //                }
+        //            }
+        //            .padding(.leading)
+        //            .padding(.trailing)
+        //
+        //
+        //        }
+        //        .background(Color("Color"))
+        //        .navigationBarTitle(dictionary.keywords, displayMode: .inline)
     }
 }
 
--- Addictech/View/MainView.swift
+++ Addictech/View/MainView.swift
@@ -20,75 +20,66 @@ struct MainView: View {
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         UITableView.appearance().contentInset.top = -35
-     
         
         
-//        UISearchBar.appearance().backgroundColor = #colorLiteral(red: 0, green: 0.1807721257, blue: 0.4193686843, alpha: 1)
-//        UITableView.appearance().backgroundColor = #colorLiteral(red: 0, green: 0.1807721257, blue: 0.4193686843, alpha: 1)
+        
+        //        UISearchBar.appearance().backgroundColor = #colorLiteral(red: 0, green: 0.1807721257, blue: 0.4193686843, alpha: 1)
+        //        UITableView.appearance().backgroundColor = #colorLiteral(red: 0, green: 0.1807721257, blue: 0.4193686843, alpha: 1)
         
     }
-   
+    
     
     var body: some View {
         NavigationView {
             ZStack{
                 Color("DarkBlue")
                     .ignoresSafeArea()
-            VStack{
-            HStack{
-                SearchBar(text: $searchText)
-                Button(action: {showInfoModalView = true
-                    
-                }) {
-                    Image(systemName: "line.horizontal.3.decrease.circle").imageScale(.large)
-                        .foregroundColor(Color.white)
-                }
-            }
-            .padding(/*@START_MENU_TOKEN@*/[.leading, .bottom, .trailing], 9.0/*@END_MENU_TOKEN@*/)
-            
-                
-            
-                ZStack{
-                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/21.0/*@END_MENU_TOKEN@*/)
-                        .foregroundColor(Color("Color"))
-                        .ignoresSafeArea()
-                List {
-                
-                    ForEach(self.fetchDictionaries.dictionaryData.filter {
-                        self.searchText.isEmpty ? true : $0.keywords.lowercased().contains(self.searchText.lowercased())
-                    }, id: \.keywords){item in
-                        NavigationLink(
-                            destination: DetailView(dictionary: item),
-                            label: {
-                                Text(item.keywords)
-                            })
+                VStack{
+                    HStack{
+                        SearchBar(text: $searchText)
+                        Button(action: {showInfoModalView = true
+                            
+                        }) {
+                            Image(systemName: "line.horizontal.3.decrease.circle").imageScale(.large)
+                                .foregroundColor(Color.white)
+                        }
                     }
+                    .padding(/*@START_MENU_TOKEN@*/[.leading, .bottom, .trailing], 9.0/*@END_MENU_TOKEN@*/)
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/21.0/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(Color("Color"))
+                            .ignoresSafeArea()
+                        List {
+                            
+                            ForEach(self.fetchDictionaries.dictionaryData.filter {
+                                self.searchText.isEmpty ? true : $0.keywords.lowercased().contains(self.searchText.lowercased())
+                            }, id: \.keywords){item in
+                                NavigationLink(
+                                    destination: DetailView(dictionary: item),
+                                    label: {
+                                        Text(item.keywords)
+                                    })
+                            }
+                        }
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .navigationTitle("Dictionary")
+                        .listStyle(GroupedListStyle())
+                    }
+                    .accentColor(Color(.label))
+                    
+                    
                 }
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-        .navigationTitle("Dictionary")
-                .listStyle(GroupedListStyle())
-                }
-                .accentColor(Color(.label))
-                
-                
             }
-        }
         }
         .sheet(isPresented: $showInfoModalView, content: {
             
-                filtermodal()
+            filtermodal()
         })
     }
 }
 
-//func makeUIViewController () -> UINavigationController {
-//    let controller = UINavigationController()
-//    let searchController = UISearchController()
-//    searchController.searchBar.placeholder = "Search"
-//    controller.navigationBar.topItem?.searchController = searchController
-//    
-//    return controller
-//}
+
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
@@ -99,4 +90,14 @@ struct MainView_Previews: PreviewProvider {
 
 //extension Color {
 //    static let darkBlue = Color(red: 00 / 255, green: 17 / 255, blue: 67 / 255)
+//}
+
+
+//func makeUIViewController () -> UINavigationController {
+//    let controller = UINavigationController()
+//    let searchController = UISearchController()
+//    searchController.searchBar.placeholder = "Search"
+//    controller.navigationBar.topItem?.searchController = searchController
+//
+//    return controller
 //}
