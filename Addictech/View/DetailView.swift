0adb823905f03e2af8232a009a094155dc36451b
Arles Asaeli <arles.halley@gmail.com>
2021-06-13 14:55:42 +0700

    coredata

--- Addictech/View/DetailView.swift
+++ Addictech/View/DetailView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct DetailView: View {
     let dictionary: Dictionary
--- Addictech/View/MainView.swift
+++ Addictech/View/MainView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreData
 // var test = "testing"
 
 struct MainView: View {
