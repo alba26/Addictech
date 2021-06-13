cc83760b70d25bf8a91c2f12a316af40afeffb82
Arles Asaeli <arles.halley@gmail.com>
2021-06-14 06:25:59 +0700

    ...

---
+++ Addictech/Model/Favorite+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Favorite+CoreDataClass.swift
+//  Addictech
+//
+//  Created by Arles Halley on 14/06/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Favorite)
+public class Favorite: NSManagedObject {
+
+}
